0b7fb8ba4cee9c2dc0695d278bf0436abdbccc84
Poul Pruitt <poul.pruitt@sendgrid.com>
2018-04-29 14:09:14 -0600

    Removed SQLAlchemy configuration

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ from resources.comment import Comment
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'poul'
 api = Api(app)
 
